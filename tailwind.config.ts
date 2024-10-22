97e85c92f5819d0cf078bff8b4057f19bb9fcd48
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-10-22 21:51:39 +0900

    Style: 색상, 자간 스타일 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,14 @@ export default {
           DEFAULT: "#212721",
         },
         gray: {
-          DEFAULT: "#9DA0A7",
+          500: "#9DA0A7", // 폰트에서 사용하는 색
+          300: "#E2E2E2", // 박스, 카드, 북마크, input 테두리 색
+          100: "#F4F4F4", // 텍스트 박스, 첨부한 이미지 테두리, hr 색
+          50: "#FCFCFC", // 배경 색
         },
+      },
+      boxShadow: {
+        box: "0 4px 6px rgba(0, 0, 0, 0.1)", // 박스 그림자
       },
     },
   },
