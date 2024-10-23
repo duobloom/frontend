39818347dc872aad19e879e56e5bac98d72c946d
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-10-23 23:49:19 +0900

    Design: 알림 컴포넌트 포인트 부분 추가 (#1)

--- src/components/common/AlarmBox.tsx
+++ src/components/common/AlarmBox.tsx
@@ -6,6 +6,8 @@ import ChevronRightSVG from "@/assets/icon/chevron-right.svg?react";
 // Props 타입 정의
 type TAlarmAlarmBoxProps = {
   children: React.ReactNode;
+  timestamp: string;
+  point?: number;
 };
 
 // 컨테이너 컴포넌트
@@ -39,20 +41,28 @@ const AlarmBoxContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HT
 );
 AlarmBoxTitle.displayName = "AlarmBoxTitle";
 
-const AlarmBox = ({ children }: TAlarmAlarmBoxProps) => {
+const AlarmBox = ({ children, timestamp, point }: TAlarmAlarmBoxProps) => {
   const navigate = useNavigate();
 
   return (
     <AlarmBoxContainer onClick={() => navigate("/alarm")}>
-      <div className="mb-[.3rem] flex items-center justify-between">
+      <div className="mb-[.5rem] flex items-center justify-between">
         <div className="flex w-[26rem] flex-col justify-start">
           <AlarmBoxTitle>듀오블룸 알리미</AlarmBoxTitle>
           {/* 해당 부분은 props로 시간을 받아야 할 것 같음 */}
-          <span className="text-[1.2rem] font-medium leading-normal tracking-tight text-gray-500">오후 01:25</span>
+          <span className="text-[1.2rem] font-medium leading-normal tracking-tight text-gray-500">{timestamp}</span>
         </div>
         <ChevronRightSVG />
       </div>
       <AlarmBoxContent>{children}</AlarmBoxContent>
+      {point && (
+        <>
+          <hr />
+          <span className="inline-block w-full text-right text-[1.3rem] font-medium leading-normal text-red">
+            {point}포인트 획득
+          </span>
+        </>
+      )}
     </AlarmBoxContainer>
   );
 };
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,5 +2,6 @@ import { Button } from "./Button";
 import { Input } from "./Input";
 import { Badge } from "./Badge";
 import InfoBox from "./InfoBox";
+import AlarmBox from "./AlarmBox";
 
-export { Button, Badge, InfoBox, Input };
+export { Button, Badge, InfoBox, Input, AlarmBox };
