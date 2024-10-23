a54aca1ae9c267cbe385cbdd0f1285b3d6548504
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-10-24 03:24:04 +0900

    Add: Post 스키마 타입 생성 (#1)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,6 @@
 import { HospitalType } from "@/types/HospitalType";
 import { PolicyType, PolicyListType } from "@/types/PolicyType";
+import { PostType } from "@/types/PostType";
+import { FeedType } from "@/types/FeedType";
 
-export { type HospitalType, type PolicyType, type PolicyListType };
+export { type HospitalType, type PolicyType, type PolicyListType, type PostType, type FeedType };
