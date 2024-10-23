2933de318dc929d2a6d0a3cc4d56e91e27ea79ec
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-10-24 03:22:28 +0900

    Design: LikeAndComments UI 컴포넌트 생성 (#1)

---
+++ src/components/ui/LikeAndComments.tsx
@@ -0,0 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import { IconComments, IconHeart } from "@/assets/icon";
+import { useState } from "react";
+
+type TLikeAndComments = {
+  type: "post" | "feed";
+  id: string;
+  likes: number;
+  comments: number;
+};
+
+const LikeAndComments = ({ type, id, likes, comments }: TLikeAndComments) => {
+  const navigate = useNavigate();
+
+  const [isLiked, setIsLiked] = useState(false); // 좋아요 클릭 여부 (서버에서 누른적 있는지 체크 하고 진행)
+  const [likeCount, setLikeCount] = useState(likes); // 현재 좋아요 개수
+
+  const handleLikeClick = () => {
+    setIsLiked((prev) => !prev); // 클릭 시 상태 반전
+    setLikeCount((prev) => (isLiked ? prev - 1 : prev + 1)); // 상태에 따라 증가/감소
+  };
+
+  return (
+    <div className="flex gap-[1.6rem]">
+      <div className="flex cursor-pointer items-center gap-[0.4rem] text-gray-500" onClick={handleLikeClick}>
+        <IconHeart className={`${isLiked ? "stroke-red" : "stroke-gray-500"}`} />
+        <span className="text-[1.4rem]">{String(likeCount)}</span>
+      </div>
+      <div
+        className="flex cursor-pointer items-center gap-[0.4rem] text-gray-500"
+        onClick={() => navigate(`${type === "feed" ? `/feed/${id}` : `/post/${id}`}`)}
+      >
+        <IconComments />
+        <span className="text-[1.4rem]">{String(comments)}</span>
+      </div>
+    </div>
+  );
+};
+
+export default LikeAndComments;
