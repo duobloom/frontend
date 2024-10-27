63de2ff77c0194e2957401ee52d4a8870a2a863b
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-10-27 17:16:37 +0900

    Add: 아이콘 추가

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,8 +1,23 @@
+import IconLogoDoubloom from "./logo_duobloom.svg?react";
 import IconHeart from "./icon-heart.svg?react";
 import IconComments from "./icon-comments.svg?react";
 import IconDotHorizontal from "./icon-dot-horizontal.svg?react";
-import IconBookMark from "./bookmark.svg?react";
-import IconChevronRight from "./chevron-right.svg?react";
-import IconLogoDoubloom from "./logo_duobloom.svg?react";
+import IconBookMark from "./icon-bookmark.svg?react";
+import IconChevronRight from "./icon-chevron-right.svg?react";
+import IconArrowLeft from "./icon-arrow-left.svg?react";
+import IconSearch from "./icon-search.svg?react";
+import IconBell from "./icon-bell.svg?react";
+import IconShare from "./icon-share.svg?react";
 
-export { IconHeart, IconComments, IconDotHorizontal, IconBookMark, IconChevronRight, IconLogoDoubloom };
+export {
+  IconHeart,
+  IconComments,
+  IconDotHorizontal,
+  IconBookMark,
+  IconChevronRight,
+  IconLogoDoubloom,
+  IconArrowLeft,
+  IconSearch,
+  IconBell,
+  IconShare,
+};
