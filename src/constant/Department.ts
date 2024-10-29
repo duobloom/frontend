0e8f13ce5908099cd7ba8fd93454c25ccffd94a4
potatoj1n <appabomul@gmail.com>
2024-10-29 11:12:01 +0900

    Add : 병원/ 클리닉 진료과 데이터 추가 (#18)

---
+++ src/constant/Department.ts
@@ -0,0 +1,20 @@
+const medicalDepartment = [
+  { id: 1, name: "전체" },
+  { id: 2, name: "산부인과" },
+  { id: 3, name: "비뇨기과" },
+  { id: 4, name: "내분비내과" },
+  { id: 5, name: "유전학과" },
+  { id: 6, name: "한방과" },
+  { id: 7, name: "정신건강의학과" },
+  { id: 8, name: "면역과" },
+  { id: 9, name: "병리학과" },
+  { id: 10, name: "영상의학과" },
+  { id: 11, name: "심장내과" },
+  { id: 12, name: "소화기내과" },
+  { id: 13, name: "재활의학과" },
+  { id: 14, name: "피부과" },
+  { id: 15, name: "정형외과" },
+  { id: 16, name: "중앙내과" },
+];
+
+export { medicalDepartment };
