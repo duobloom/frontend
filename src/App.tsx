c7fa92a947855c4d4945bd7acd339e2fe7a32477
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-10-31 22:35:26 +0900

    Refactor: App.tsx 앱 레이아웃 리팩토링 (#19)

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import Navbar from "./components/layout/Nav";
 function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-[#fff5f5]">
-      <div className="container relative flex max-w-[120rem] justify-center">
+      <div className="container relative flex max-w-[100rem] justify-between xl:max-w-[120rem]">
         {/* Left section - 로고 영역 */}
-        <div className="hidden lg:block lg:w-[50%] xl:w-[55%]">
-          <div className="relative h-screen w-full max-w-[49rem] pt-[5.1rem]">
-            <div className="mb-20">
+        <div className="notebook:w-[82.5rem] hidden lg:block">
+          <div className="relative h-screen w-full max-w-[50rem] pt-[5rem] xl:pl-[10.2rem]">
+            <div className="mb-[14.7rem]">
               <IconLogoDoubloom />
             </div>
-            <div className="space-y-2 text-[4.6rem] font-medium leading-[1.2] tracking-[-2px]">
+            <div className="space-y-[0.2rem] text-[4.6rem] font-medium leading-[1.2] tracking-[-0.2rem]">
               <h1>소중한 길을</h1>
               <h1>따뜻하게 동행하는</h1>
               <h1 className="font-black text-red">듀오블룸</h1>
@@ -21,8 +21,8 @@ function App() {
         </div>
 
         {/* Right section - 앱 영역 */}
-        <div className="flex w-full justify-center lg:relative lg:w-[37.5rem]">
-          <div className="right-1/4 top-0 h-screen w-full max-w-[37.5rem] bg-white shadow-lg lg:fixed lg:right-auto">
+        <div className="right-section flex w-full justify-center lg:w-[37.5rem] lg:min-w-[37.5rem]">
+          <div className="h-screen w-full max-w-[37.5rem] bg-white shadow-lg">
             <div className="flex h-full flex-col">
               <div className="flex-1 overflow-y-auto will-change-scroll">
                 <Outlet />
