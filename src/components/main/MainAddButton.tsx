0e12ee033a21ab69bb5a8d90f238a5fe6df9f0e2
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-02 00:39:16 +0900

    Design: 메인 페이지 하단 +버튼 마크업 및 디자인 (#17)

--- src/components/main/MainAddButton.tsx
+++ src/components/main/MainAddButton.tsx
@@ -1,50 +1,72 @@
 import React, { useState } from "react";
-import { cn } from "@/utils";
 import { MainTextForm, MainEmotionForm } from "@/components/main";
+import { Drawer, DrawerContent, DrawerTrigger } from "../common/Drawer";
+import { cn } from "@/utils";
 import { IconEdit, IconPlus, IconSmileHappy } from "@/assets/icon";
 
 // 아이콘 버튼 컨테이너
-const IconButtonContainer = React.forwardRef<HTMLButtonElement, React.HTMLAttributes<HTMLButtonElement>>(
-  ({ className, children, ...props }) => (
+const IconButtonContainer = ({
+  className,
+  children,
+  onClick,
+}: {
+  className: string;
+  children: React.ReactNode;
+  onClick?: () => void;
+}) => {
+  return (
     <button
       className={cn(
         "absolute right-[2rem] flex h-[5rem] w-[5rem] transform items-center justify-center rounded-full border border-gray-300",
         className,
       )}
-      {...props}
+      onClick={onClick}
     >
       {children}
     </button>
-  ),
-);
-IconButtonContainer.displayName = "IconButtonContainer";
+  );
+};
 
 const MainAddButton = () => {
   const [isClicked, setIsClicked] = useState(false);
+  const [isTextDrawerOpen, setIsTextDrawerOpen] = useState(false);
+  const [isEmotionDrawerOpen, setIsEmotionDrawerOpen] = useState(false);
 
   return (
     <div>
       {/* 글쓰기 버튼 */}
-      <MainTextForm>
-        <IconButtonContainer
-          className={`bottom-[13rem] bg-white transition-all duration-300 ease-out ${
-            isClicked ? "translate-y-0 opacity-100" : "pointer-events-none translate-y-8 opacity-0"
-          }`}
-        >
-          <IconEdit />
-        </IconButtonContainer>
-      </MainTextForm>
+      <Drawer open={isTextDrawerOpen} onOpenChange={setIsTextDrawerOpen}>
+        <DrawerTrigger asChild>
+          <IconButtonContainer
+            onClick={() => setIsClicked(false)}
+            className={`bottom-[13rem] bg-white transition-all duration-300 ease-out ${
+              isClicked ? "translate-y-0 opacity-100" : "pointer-events-none translate-y-8 opacity-0"
+            }`}
+          >
+            <IconEdit />
+          </IconButtonContainer>
+        </DrawerTrigger>
+        <DrawerContent>
+          <MainTextForm onClose={() => setIsTextDrawerOpen(false)} />
+        </DrawerContent>
+      </Drawer>
 
       {/* 감정 추가 버튼 */}
-      <MainEmotionForm>
-        <IconButtonContainer
-          className={`bottom-[7.5rem] bg-white transition-all duration-300 ease-out ${
-            isClicked ? "translate-y-0 opacity-100" : "pointer-events-none translate-y-8 opacity-0"
-          }`}
-        >
-          <IconSmileHappy />
-        </IconButtonContainer>
-      </MainEmotionForm>
+      <Drawer open={isEmotionDrawerOpen} onOpenChange={setIsEmotionDrawerOpen}>
+        <DrawerTrigger asChild>
+          <IconButtonContainer
+            onClick={() => setIsClicked(false)}
+            className={`bottom-[7.5rem] bg-white transition-all duration-300 ease-out ${
+              isClicked ? "translate-y-0 opacity-100" : "pointer-events-none translate-y-8 opacity-0"
+            }`}
+          >
+            <IconSmileHappy />
+          </IconButtonContainer>
+        </DrawerTrigger>
+        <DrawerContent>
+          <MainEmotionForm onClose={() => setIsEmotionDrawerOpen(false)} />
+        </DrawerContent>
+      </Drawer>
 
       {/* + 버튼 */}
       <IconButtonContainer
