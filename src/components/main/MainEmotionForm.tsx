35be683946dad77a4d1121771f9212f862d2cd2b
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-02 00:39:46 +0900

    Design: 감정 작성 시트 마크업 및 디자인 (#17)

--- src/components/main/MainEmotionForm.tsx
+++ src/components/main/MainEmotionForm.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Drawer, DrawerClose, DrawerContent, DrawerTitle, DrawerTrigger } from "../common/Drawer";
+import { useState } from "react";
+import { DrawerClose, DrawerTitle } from "../common/Drawer";
 import { Button } from "../common";
 
 const emotionList = [
@@ -13,45 +13,41 @@ const emotionList = [
   { id: 8, text: "축하할 일이 있을 땐 마음껏 즐기자!", emoji: "🥳" },
 ];
 
-const MainEmotionForm = ({ emojiNum = 0, children }: { emojiNum?: number; children: React.ReactNode }) => {
+const MainEmotionForm = ({ emojiNum = 0, onClose }: { emojiNum?: number; onClose: () => void }) => {
   const [clickEmojiNum, setClickEmojiNum] = useState(emojiNum); // 감정을 클릭한 적 있다면
-  const [isOpen, setIsOpen] = useState(false);
 
   const handleSubmit = () => {
     console.log(clickEmojiNum);
-    setIsOpen(false);
     setClickEmojiNum(0); // API 통신 완료 시
+    onClose();
   };
 
   return (
-    <Drawer open={isOpen} onOpenChange={setIsOpen}>
-      <DrawerTrigger asChild>{children}</DrawerTrigger>
-      <DrawerContent>
-        <div className="flex items-center justify-between pb-[2rem]">
-          <DrawerClose />
-          <DrawerTitle text="내 감정" />
-          <Button variant="oval" size="sm" disabled={clickEmojiNum === emojiNum} onClick={handleSubmit}>
-            완료
-          </Button>
-        </div>
-        <div className="flex flex-col gap-[1rem]">
-          {emotionList.map((item) => (
-            <div
-              key={item.id}
-              onClick={() => setClickEmojiNum(item.id)}
-              className={`flex h-[6.6rem] cursor-pointer items-center justify-between rounded-[1rem] border px-[2rem] py-[1.5rem] ${clickEmojiNum === item.id ? "bg-red-10 border-red" : "border-gray-300 bg-white"} shadow-box`}
+    <>
+      <div className="flex items-center justify-between pb-[1.4rem]">
+        <DrawerClose onClick={() => setClickEmojiNum(0)} />
+        <DrawerTitle text="내 감정" />
+        <Button variant="oval" size="sm" disabled={clickEmojiNum === emojiNum} onClick={handleSubmit}>
+          완료
+        </Button>
+      </div>
+      <div className="flex flex-col gap-[1rem]">
+        {emotionList.map((item) => (
+          <div
+            key={item.id}
+            onClick={() => setClickEmojiNum(item.id)}
+            className={`flex h-[6.6rem] cursor-pointer items-center justify-between rounded-[1rem] border px-[2rem] py-[1.5rem] ${clickEmojiNum === item.id ? "border-red bg-red-10" : "border-gray-300 bg-white"} shadow-box`}
+          >
+            <p
+              className={`w-[25rem] text-[1.4rem] font-semibold leading-[1.8rem] tracking-[-0.028rem] ${clickEmojiNum === item.id ? "text-red" : "text-gray-500"}`}
             >
-              <p
-                className={`w-[25rem] text-[1.4rem] font-semibold leading-[1.8rem] tracking-[-0.028rem] ${clickEmojiNum === item.id ? "text-red" : "text-gray-500"}`}
-              >
-                {item.text}
-              </p>
-              <span className="flex h-[3.6rem] w-[3.6rem] items-end justify-center text-[2.5rem]">{item.emoji}</span>
-            </div>
-          ))}
-        </div>
-      </DrawerContent>
-    </Drawer>
+              {item.text}
+            </p>
+            <span className="flex h-[3.6rem] w-[3.6rem] items-end justify-center text-[2.5rem]">{item.emoji}</span>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
