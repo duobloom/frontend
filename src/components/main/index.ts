b54c291aa39cb5d5935aa25f8112e0301d384d82
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-02 00:38:29 +0900

    Design: 메인 페이지 레이아웃 구성 (#17)

--- src/components/main/index.ts
+++ src/components/main/index.ts
@@ -1,5 +1,9 @@
 import MainDayAndPoint from "./MainDayAndPoint";
 import MainProfile from "./MainProfile";
 import MainCalendar from "./MainCalendar";
+import MainFeed from "./MainFeed";
+import MainAddButton from "./MainAddButton";
+import MainTextForm from "./MainTextForm";
+import MainEmotionForm from "./MainEmotionForm";
 
-export { MainDayAndPoint, MainProfile, MainCalendar };
+export { MainDayAndPoint, MainProfile, MainCalendar, MainFeed, MainAddButton, MainTextForm, MainEmotionForm };
