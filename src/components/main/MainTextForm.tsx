94316d9260769eb9562f2790296c9c91cb4d789d
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-04 02:44:32 +0900

    Feat: 글 추가 컴포넌트 Alert Dialog 추가 및 글 수정 분기 (#17)

--- src/components/main/MainTextForm.tsx
+++ src/components/main/MainTextForm.tsx
@@ -1,17 +1,37 @@
 import React, { useEffect, useRef, useState } from "react";
-import { DrawerClose, DrawerTitle } from "../common/Drawer";
+import { DrawerTitle } from "../common/Drawer";
 import { Button } from "../common";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/common/AlertDialog";
 import useDraggable from "@/hooks/useDraggable";
-import { IconCamera, IconCircleClose } from "@/assets/icon";
+import { IconCamera, IconCircleClose, IconClose } from "@/assets/icon";
+import { FeedPostType } from "@/types/FeedType";
 
-const MainTextForm = ({ onClose }: { onClose: () => void }) => {
+const MainTextForm = ({
+  type = "add",
+  initialData = null,
+  onClose,
+}: {
+  type?: string;
+  initialData?: FeedPostType | null;
+  onClose: () => void;
+}) => {
   const fileRef = useRef<HTMLInputElement>(null);
   const textRef = useRef<HTMLTextAreaElement>(null);
   const scrollRef = useRef<HTMLDivElement>(null);
   const draggableOptions = useDraggable(scrollRef);
 
-  const [imgList, setImgList] = useState<{ img_id: string; img_src: string }[]>([]);
+  const [imgList, setImgList] = useState<{ url: string; alt?: string }[]>(initialData?.images || []);
   const [isFormValid, setIsFormValid] = useState(false);
+  const [showConfirmDialog, setShowConfirmDialog] = useState(false);
 
   useEffect(() => {
     if (textRef.current) {
@@ -43,8 +63,8 @@ const MainTextForm = ({ onClose }: { onClose: () => void }) => {
   };
 
   // 이미지 삭제
-  const handleImgDelete = (img_id: string) => {
-    const newList = imgList.filter((img) => img.img_id !== img_id);
+  const handleImgDelete = (imgUrl: string) => {
+    const newList = imgList.filter((img) => img.url !== imgUrl);
     setImgList(newList);
     if (textRef.current) {
       setIsFormValid(textRef.current.value.trim().length > 0 || newList.length > 0);
@@ -54,13 +74,13 @@ const MainTextForm = ({ onClose }: { onClose: () => void }) => {
   // 이미지 추가
   const handleImgAdd = (e: React.ChangeEvent<HTMLInputElement>) => {
     const target = e.currentTarget;
-    const files = (target.files as FileList)[0];
+    const files = target.files;
 
-    if (files === undefined) return;
+    if (!files || files.length === 0) return;
 
     const newImage = {
-      img_id: Date.now().toString(),
-      img_src: URL.createObjectURL(files),
+      url: URL.createObjectURL(files[0]), // img_src를 url로 변경
+      alt: "", // 필요한 경우 alt를 여기에 설정할 수 있습니다.
     };
     setImgList([...imgList, newImage]);
     setIsFormValid(true);
@@ -75,11 +95,29 @@ const MainTextForm = ({ onClose }: { onClose: () => void }) => {
     setIsFormValid(false);
   };
 
+  // 닫기 전 확인
+  const handleClose = () => {
+    if (isFormValid) {
+      setShowConfirmDialog(true);
+    } else {
+      resetForm();
+      onClose();
+    }
+  };
+
+  const handleConfirmClose = () => {
+    resetForm();
+    onClose();
+    setShowConfirmDialog(false);
+  };
+
   return (
     <>
       <div className="flex items-center justify-between pb-[1.4rem]">
-        <DrawerClose onClick={resetForm} />
-        <DrawerTitle text="글 쓰기" />
+        <div onClick={handleClose} className="cursor-pointer">
+          <IconClose />
+        </div>
+        <DrawerTitle text={`${type === "add" ? "글 쓰기" : "글 수정"}`} />
         <Button variant="oval" size="sm" disabled={!isFormValid} onClick={handleSubmit}>
           완료
         </Button>
@@ -91,13 +129,13 @@ const MainTextForm = ({ onClose }: { onClose: () => void }) => {
       >
         {imgList.map((img) => (
           <div
-            key={img.img_id}
+            key={img.url}
             className="relative h-[10.5rem] w-[10.5rem] min-w-[10.5rem] overflow-hidden rounded-[1rem] border border-gray-300"
           >
-            <img src={img.img_src} alt="이미지" className="h-full w-full object-cover" />
+            <img src={img.url} alt={img.alt || "이미지"} className="h-full w-full object-cover" />
             <IconCircleClose
               className="absolute right-[.4rem] top-[.4rem] cursor-pointer"
-              onClick={() => handleImgDelete(img.img_id)}
+              onClick={() => handleImgDelete(img.url)}
             />
           </div>
         ))}
@@ -112,8 +150,22 @@ const MainTextForm = ({ onClose }: { onClose: () => void }) => {
       <textarea
         ref={textRef}
         placeholder="내용을 입력해 주세요"
+        defaultValue={initialData?.content}
         className="h-full resize-none text-[1.4rem] font-medium leading-[2rem] tracking-[-0.028] text-black outline-none"
       ></textarea>
+
+      <AlertDialog open={showConfirmDialog} onOpenChange={setShowConfirmDialog}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>알림</AlertDialogTitle>
+            <AlertDialogDescription>글 작성을 취소하시겠어요?</AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>아니오</AlertDialogCancel>
+            <AlertDialogAction onClick={handleConfirmClose}>네</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   );
 };
