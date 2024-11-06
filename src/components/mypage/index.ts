b5e6474eec2aa594f2a95e232a66b5dc040a0fe7
potatoj1n <appabomul@gmail.com>
2024-11-06 17:05:57 +0900

    feat : 내 정보 수정 입력 필드 컴포넌트 생성(#26)

--- src/components/mypage/index.ts
+++ src/components/mypage/index.ts
@@ -1,4 +1,6 @@
 import PointContainer from "./PointContainer";
 import { NavigateBox, AlarmBox } from "./NavigateBox";
+import { EditInput } from "./EditInput";
+import TransactionBox from "./TransactionBox";
 
-export { PointContainer, NavigateBox, AlarmBox };
+export { PointContainer, NavigateBox, AlarmBox, EditInput, TransactionBox };
