d2ecd3cbfebe91579ee066dd58487d1215edad93
potatoj1n <appabomul@gmail.com>
2024-11-06 17:24:54 +0900

    refactor : 유저 스키마 수정(#26)

--- src/types/UserType.ts
+++ src/types/UserType.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
 export const AuthorSchema = z.object({
-  user_id: z.number(),
+  user_id: z.number().int(),
   name: z.string(),
-  birth: z.string(),
+  birth: z.string().optional(),
+  balance: z.number().default(0),
+  couple_user_id: z.number().int(),
   profileImage: z.string().optional(),
 });
 
