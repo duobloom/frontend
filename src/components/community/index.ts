162913b1090815ee44488d1bc087787e98d3a961
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-07 22:29:31 +0900

    Refactor: 커뮤니티 페이지 구조 변경 (#30)

--- src/components/community/index.ts
+++ src/components/community/index.ts
@@ -2,5 +2,6 @@ import CommunityTitle from "./CommunityTitle";
 import CommunityOpenChat from "./CommunityOpenChat";
 import CommunityMagazine from "./CommunityMagazine";
 import CommunityPostList from "./CommunityPostList";
+import CommunityWriteButton from "./CommunityWriteButton";
 
-export { CommunityTitle, CommunityOpenChat, CommunityMagazine, CommunityPostList };
+export { CommunityTitle, CommunityOpenChat, CommunityMagazine, CommunityPostList, CommunityWriteButton };
