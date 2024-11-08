eb1330ab304d7f8ac59f53eb2631260e9ea38984
potatoj1n <appabomul@gmail.com>
2024-11-09 01:44:02 +0900

    feat : 병원 지역 필터링 useQuery 추가 및 데이터 적용(#18)

--- src/pages/HospitalPage.tsx
+++ src/pages/HospitalPage.tsx
@@ -2,81 +2,93 @@ import { useState } from "react";
 import { Drawer, DrawerContent, DrawerTrigger, DrawerTitle, DrawerClose } from "@/components/common/Drawer";
 import Header from "@/components/layout/Header";
 import { BoxFooter, DropdownBox } from "@/components/ui/Box";
-import { medicalDepartment, medicalOptions, regions } from "@/constants";
+import { medicalDepartment, medicalOptions } from "@/constants";
 import { Button, OptionBoxes, ScrollableOptions } from "@/components/common";
 import { IconMap } from "@/assets/icon";
 import { useNavigate } from "react-router-dom";
-import { HospitalInfo, exampleHospitalData, RegionSelecter } from "@/components/hospital";
+import { GetRegionName, HospitalInfo, RegionSelecter } from "@/components/hospital";
+import { getSearchHospitalRegion } from "@/apis";
+import { useQuery } from "@tanstack/react-query";
 
 const HospitalPage = () => {
   const navigate = useNavigate();
   const [activeDrawer, setActiveDrawer] = useState<"location" | "department" | null>(null);
   const [selectedOption, setSelectedOption] = useState(1);
   const [selectedDepartment, setSelectedDepartment] = useState(1);
-  const [selectedSi, setSelectedSi] = useState<number | null>(1);
-  const [selectedGun, setSelectedGun] = useState<number | null>(null);
+  const [selectedSi, setSelectedSi] = useState<number | null>(0);
+  const [selectedGun, setSelectedGun] = useState<number | null>(0);
   const [selectedNeighborhood, setSelectedNeighborhood] = useState<number | null>(null);
 
+  const {
+    data: hospitalData,
+    isError,
+    error,
+    refetch: refetchHospital,
+  } = useQuery({
+    queryKey: ["hospitalData", selectedSi, selectedGun, selectedNeighborhood],
+    queryFn: () => getSearchHospitalRegion(selectedSi, selectedGun, selectedNeighborhood),
+    enabled: false,
+  });
+  const applyFilters = () => {
+    if (activeDrawer === "location") refetchHospital();
+    //else 진료과 필터링;
+    setActiveDrawer(null);
+  };
+
+  if (isError) return <>{error.message}</>;
+
   const selectedDepartmentName = medicalDepartment.find((department) => department.id === selectedDepartment)?.name;
-  const selectedLocation = `${selectedSi ? regions.find((r) => r.id === selectedSi)?.si : ""} ${
-    selectedGun ? "· " + regions.find((r) => r.id === selectedSi)?.gun.find((g) => g.id === selectedGun)?.name : ""
-  } ${
-    selectedNeighborhood
-      ? "· " +
-        regions
-          .find((r) => r.id === selectedSi)
-          ?.gun.find((g) => g.id === selectedGun)
-          ?.neighborhoods.find((n) => n.id === selectedNeighborhood)?.name
-      : ""
-  }`;
 
   return (
-    <div className="flex w-full flex-col">
+    <div className="flex h-full w-full flex-col">
       <Header variant="titleMove" title="병원/클리닉" />
       <Drawer>
-        <span className="flex w-full items-center justify-between">
+        <span className="flex w-full items-center justify-between px-[1.5rem]">
           <DrawerTrigger onClick={() => setActiveDrawer("location")}>
-            <DropdownBox>{selectedLocation || "지역 선택"}</DropdownBox>
+            <DropdownBox>
+              {selectedGun ? (
+                <GetRegionName
+                  selectedSi={selectedSi}
+                  selectedGun={selectedGun}
+                  selectedNeighborhood={selectedNeighborhood}
+                />
+              ) : (
+                "전체"
+              )}
+            </DropdownBox>
           </DrawerTrigger>
           <DrawerTrigger onClick={() => setActiveDrawer("department")}>
-            <DropdownBox>{selectedDepartmentName || "진료과 선택"}</DropdownBox>
+            <DropdownBox>{selectedDepartmentName || "전체"}</DropdownBox>
           </DrawerTrigger>
         </span>
         <ScrollableOptions options={medicalOptions} selectedOption={selectedOption} onSelect={setSelectedOption} />
-        <div className="px-[1.5rem]">
-          <p className="mt-[1rem] text-[1.5rem] font-medium">190{}개의 병원/클리닉</p>
+        <div className="flex-1 px-[1.5rem]">
+          <p className="mt-[1rem] text-[1.5rem] font-medium">
+            {hospitalData ? `${hospitalData.length}개의 병원/클리닉` : "0 개의 병원/클리닉"}
+          </p>
           <BoxFooter />
-          <HospitalInfo item={exampleHospitalData} />
+          {hospitalData && hospitalData.length > 0 ? (
+            hospitalData.map((item) => <HospitalInfo key={item.hospital_id} item={item} />)
+          ) : (
+            <p className="text-center text-[1.5rem] text-gray-400">데이터가 없습니다.</p>
+          )}
         </div>
-        <Button
-          variant="ovalReverse"
-          size="md"
-          className="absolute bottom-[8rem] right-[13.5rem]"
-          onClick={() => navigate("map")}
-        >
+        <Button variant="ovalReverse" size="md" className="mb-[1rem] self-center" onClick={() => navigate("map")}>
           <IconMap />
           지도보기
         </Button>
-        {/* Drawer Content */}
+
         <DrawerContent>
-          {/* 지역 */}
           {activeDrawer === "location" && (
-            <>
-              <span className="relative mb-[3.5rem] flex w-full items-center">
-                <DrawerClose className="absolute left-0" />
-                <DrawerTitle text="지역 선택" className="mx-auto" />
-              </span>
-              <RegionSelecter
-                selectedSi={selectedSi}
-                setSelectedSi={setSelectedSi}
-                selectedGun={selectedGun}
-                setSelectedGun={setSelectedGun}
-                selectedNeighborhood={selectedNeighborhood}
-                setSelectedNeighborhood={setSelectedNeighborhood}
-              />
-            </>
+            <RegionSelecter
+              selectedSi={selectedSi}
+              setSelectedSi={setSelectedSi}
+              selectedGun={selectedGun}
+              setSelectedGun={setSelectedGun}
+              selectedNeighborhood={selectedNeighborhood}
+              setSelectedNeighborhood={setSelectedNeighborhood}
+            />
           )}
-          {/* 진료과 */}
           {activeDrawer === "department" && (
             <>
               <span className="relative mb-[3.5rem] flex w-full items-center">
@@ -92,7 +104,12 @@ const HospitalPage = () => {
           )}
           <span className="fixed bottom-[1rem] left-0 w-full px-[2rem]">
             <BoxFooter />
-            <Button variant="default" className="w-full">
+            <Button
+              variant="default"
+              className="w-full"
+              onClick={() => applyFilters()}
+              disabled={selectedSi === null || selectedGun === null}
+            >
               적용하기
             </Button>
           </span>
