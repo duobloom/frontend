558014ece674fa7669175a35570a7e87c86f2a9a
potatoj1n <appabomul@gmail.com>
2024-11-09 23:52:40 +0900

    feat : 병원 정보 필터링 요청 API 생성-지역, 타입, 진료과(#18)

--- src/apis/hospital/getFilterHospital.ts
+++ src/apis/hospital/getFilterHospital.ts
@@ -5,13 +5,22 @@ export const getFilterHospital = async (
   region_code?: number | null,
   middle_code?: number | null,
   detail_code?: number | null,
+  option?: string | null,
+  department?: string | null,
 ) => {
   const params = {
-    region: region_code,
-    middle: middle_code,
+    ...(region_code ? { region: region_code } : {}),
+    ...(middle_code ? { middle: middle_code } : {}),
     ...(detail_code ? { detail: detail_code } : {}),
+    ...(option ? { keyword: option } : {}),
+    ...(department ? { type: department } : {}),
   };
 
-  const response = await axios.get<HospitalType[]>("/hospitals/search", { params });
+  const response = await axios.get<HospitalType[]>(
+    "http://ec2-52-78-212-34.ap-northeast-2.compute.amazonaws.com:8080/api/hospitals/search",
+    { params },
+  );
+  console.log(params);
+  console.log(response.data);
   return response.data;
 };
