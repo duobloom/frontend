89cdce13cd4b9368de035911d9537b0632e430b1
potatoj1n <appabomul@gmail.com>
2024-11-11 22:54:15 +0900

    refactor : 유저 정보 스키마 리팩토링(#39)

--- src/types/UserType.ts
+++ src/types/UserType.ts
@@ -1,16 +1,23 @@
 import { z } from "zod";
 
 export const AuthorSchema = z.object({
-  user_id: z.number().int(),
-  name: z.string(),
+  userId: z.number().int(),
+  nickname: z.string(),
   birth: z.string().optional(),
   balance: z.number().default(0).optional(),
-  couple_user_id: z.number().int().optional(),
+  coupleBalance: z.number().default(0).optional(),
+  coupleUserId: z.number().int().optional(),
   profileImage: z.string().optional(),
 });
 
 export const UserSchema = z.object({
-  author: AuthorSchema,
+  userId: z.number().int(),
+  nickname: z.string(),
+  birth: z.string().optional(),
+  balance: z.number().default(0).optional(),
+  coupleBalance: z.number().default(0).optional(),
+  coupleUserId: z.number().int().optional(),
+  profileImage: z.string().optional(),
   email: z.string().email("올바른 이메일 형식이 아닙니다"),
   region: z.string(),
 });
