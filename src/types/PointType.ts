c60b4bd9523648623951782838274616082cf877
potatoj1n <appabomul@gmail.com>
2024-11-11 10:50:28 +0900

    refactor : 포인트 거래내역 타입 리팩토링(#39)

--- src/types/PointType.ts
+++ src/types/PointType.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
 export const PointSchema = z.object({
-  transaction_id: z.number().int(),
+  transactionId: z.number().int(),
   amount: z.number().int(),
-  transaction_type: z.enum(["ANSWER", "CHALLENGE_REWARD"]),
-  created_at: z.string().datetime(),
+  transactionType: z.enum(["ANSWER", "CHALLENGE_REWARD"]),
+  createdAt: z.string().datetime(),
   balance: z.number(),
-  user_id: z.number().int(),
+  userId: z.number().int(),
 });
 
 export type PointType = z.infer<typeof PointSchema>;
