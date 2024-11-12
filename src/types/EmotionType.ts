f47193cc516d432cf3ead6ed2a5297887fa397b9
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-13 02:48:50 +0900

    Refactor: 감정 타입 수정 (#39)

--- src/types/EmotionType.ts
+++ src/types/EmotionType.ts
@@ -1,12 +1,14 @@
 import { z } from "zod";
-import { AuthorSchema } from "./UserType";
 
 // 감정 타입
 export const EmotionSchema = z.object({
   emotionId: z.number(),
   emoji: z.number(),
   feedDate: z.string(),
-  author: AuthorSchema.optional(),
+  createdAt: z.string(),
+  authorNickname: z.string(),
+  authorProfilePictureUrl: z.string(),
+  mine: z.boolean().default(false),
 });
 
 export type EmotionType = z.infer<typeof EmotionSchema>;
