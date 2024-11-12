c7a6b313f47c8f862583878d90e3515d6c37fed9
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-13 02:45:23 +0900

    Feat: 날짜 변환 유틸 함수 생성

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,6 @@ import { cn } from "./cn";
 import dDayCalculation from "./dDayCalculation";
 import { generateDates } from "./generateDates";
 import reduceImageSize from "./imageResize";
+import { formatDateConvert } from "./formatDateConvert";
 
-export { cn, dDayCalculation, generateDates, reduceImageSize };
+export { cn, dDayCalculation, generateDates, reduceImageSize, formatDateConvert };
