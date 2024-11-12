4c723f3f66ea3289067eeef446ada757134c5acf
potatoj1n <appabomul@gmail.com>
2024-11-12 12:04:37 +0900

    Add : 유저 정보 수정 api 추가(#26)

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,6 @@
 import { getFilterHospital } from "./hospital/getFilterHospital";
 import { getPointTransaction } from "./mypage/getPointTransaction";
 import { getMyProfile } from "./mypage/getMyProfile";
+import { patchMyProfile } from "./mypage/patchMyProfile";
 
-export { getFilterHospital, getPointTransaction, getMyProfile };
+export { getFilterHospital, getPointTransaction, getMyProfile, patchMyProfile };
