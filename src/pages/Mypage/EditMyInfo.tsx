83cd21f733a019bb8d20574adb0b924ce5f6f26d
potatoj1n <appabomul@gmail.com>
2024-11-13 23:14:30 +0900

    feat : 내 정보 수정 API 요청 및 데이터 적용 & 이미지 업로드, 리사이즈 함수 적용(#26)

--- src/pages/Mypage/EditMyInfo.tsx
+++ src/pages/Mypage/EditMyInfo.tsx
@@ -1,26 +1,45 @@
 import { Button } from "@/components/common";
 import Header from "@/components/layout/Header";
 import { EditInput } from "@/components/mypage";
+import { useUpdateProfile } from "@/hooks/useUpdateProfile";
 import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { getS3Url, postPresignedUrl, putS3Upload } from "@/apis/image/imageUpload";
+import { reduceImageSize } from "@/utils";
 
 const EditMyInfo = () => {
+  const location = useLocation();
+  const userInfo = location.state?.userInfo;
   const fileRef = React.useRef<HTMLInputElement>(null);
-  const [profileImage, setProfileImage] = useState<string>("https://example.com/profile-image.jpg");
-  const [nickname, setNickname] = useState("");
-  const [email, setEmail] = useState("");
-  const [birthdate, setBirthdate] = useState("");
-  const [location, setLocation] = useState("");
+  const [profilePictureUrl, setProfilePictureUrl] = useState<string>(userInfo.profilePictureUrl);
+  const [nickname, setNickname] = useState(userInfo.nickname);
+  const [email, setEmail] = useState(userInfo.email);
+  const [birth, setBirth] = useState(userInfo.birth);
+  const [region, setRegion] = useState(userInfo.region);
+  const patchMutate = useUpdateProfile();
 
-  const handleImgEdit = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleImgEdit = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
-      const imageUrl = URL.createObjectURL(file);
-      setProfileImage(imageUrl);
+      try {
+        const compressedBlob = await reduceImageSize(URL.createObjectURL(file));
+        const presignedUrls = await postPresignedUrl([file.name]);
+
+        await putS3Upload(presignedUrls, [{ photo_url: file.name, file: compressedBlob as File }]);
+        const s3ImageUrl = getS3Url(presignedUrls[0]);
+        setProfilePictureUrl(s3ImageUrl);
+      } catch (error) {
+        console.error("이미지 업로드 중 오류가 발생했습니다:", error);
+      }
     }
   };
 
   const handleEditClick = () => {
     fileRef.current?.click();
+  };
+
+  const handleApply = () => {
+    patchMutate.mutate({ nickname, birth, profilePictureUrl, email, region });
   };
 
   return (
@@ -29,7 +48,7 @@ const EditMyInfo = () => {
       <div className="flex-1 px-[1.5rem] pt-[2rem]">
         <div className="flex flex-col items-center justify-center">
           <img
-            src={profileImage}
+            src={profilePictureUrl}
             alt="Profile"
             className="relative h-[12rem] w-[12rem] rounded-full border border-gray-300 object-cover"
           />
@@ -43,30 +62,37 @@ const EditMyInfo = () => {
           <h1 className="text-[1.5rem] font-bold">닉네임</h1>
           <EditInput
             variant="nickname"
-            value={nickname}
+            value={nickname ?? ""}
             onChange={(e) => setNickname(e.target.value)}
             placeholder="닉네임"
           />
           <h1 className="mt-[1rem] text-[1.5rem] font-bold">이메일</h1>
-          <EditInput variant="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="이메일" />
+          <EditInput
+            variant="email"
+            value={email ?? ""}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="이메일"
+          />
           <h1 className="mt-[1rem] text-[1.5rem] font-bold">생년월일</h1>
           <EditInput
             variant="birthdate"
-            value={birthdate}
-            onChange={(e) => setBirthdate(e.target.value)}
-            placeholder="생년월일"
+            value={birth ?? ""}
+            onChange={(e) => setBirth(e.target.value)}
+            placeholder="YYYY-MM-DD"
           />
           <h1 className="mt-[1rem] text-[1.5rem] font-bold">거주 지역</h1>
           <EditInput
             variant="location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-            placeholder="거주 지역"
+            value={region ?? ""}
+            onChange={(e) => setRegion(e.target.value)}
+            placeholder="서울시 강남구"
           />
         </section>
       </div>
       <footer className="flex w-full items-center gap-[.7rem] border-t border-gray-300 py-[1rem]">
-        <Button className="mx-[1.5rem]">적용하기</Button>
+        <Button className="mx-[1.5rem]" onClick={handleApply}>
+          적용하기
+        </Button>
       </footer>
     </div>
   );
