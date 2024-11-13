3dd2df72403de18c0868cb6b9bb5be2ff6f45343
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-13 16:57:53 +0900

    Refactor: 타입 수정에 따른 좋아요, 댓글 부분 수정 (#1 #17)

--- src/components/ui/LikeAndComments.tsx
+++ src/components/ui/LikeAndComments.tsx
@@ -1,38 +1,37 @@
 import { useNavigate } from "react-router-dom";
 import { IconComments, IconHeart } from "@/assets/icon";
 import { useState } from "react";
-import { CommentType } from "@/types/CommentType";
 
 type TLikeAndComments = {
   type: "community" | "board";
   id: number;
-  likes: number;
-  comments: CommentType[];
+  likeCount: number;
+  commentCount: number;
 };
 
-const LikeAndComments = ({ type, id, likes, comments }: TLikeAndComments) => {
+const LikeAndComments = ({ type, id, likeCount, commentCount }: TLikeAndComments) => {
   const navigate = useNavigate();
 
   const [isLiked, setIsLiked] = useState(false); // 좋아요 클릭 여부 (서버에서 누른적 있는지 체크 하고 진행)
-  const [likeCount, setLikeCount] = useState(likes); // 현재 좋아요 개수
+  const [likeCountNum, setLikeCountNum] = useState(likeCount); // 현재 좋아요 개수
 
   const handleLikeClick = () => {
     setIsLiked((prev) => !prev); // 클릭 시 상태 반전
-    setLikeCount((prev) => (isLiked ? prev - 1 : prev + 1)); // 상태에 따라 증가/감소
+    setLikeCountNum((prev) => (isLiked ? prev - 1 : prev + 1)); // 상태에 따라 증가/감소
   };
 
   return (
     <div className="flex gap-[1.6rem]">
       <div className="flex cursor-pointer items-center gap-[0.4rem] text-gray-500" onClick={handleLikeClick}>
         <IconHeart className={`${isLiked ? "stroke-red" : "stroke-gray-500"}`} />
-        <span className="text-[1.4rem]">{String(likeCount)}</span>
+        <span className="text-[1.4rem]">{String(likeCountNum)}</span>
       </div>
       <div
         className="flex cursor-pointer items-center gap-[0.4rem] text-gray-500"
         onClick={() => navigate(`${type === "board" ? `/board/${id}` : `/community/${id}`}`)}
       >
         <IconComments />
-        <span className="text-[1.4rem]">{String(comments.length)}</span>
+        <span className="text-[1.4rem]">{String(commentCount)}</span>
       </div>
     </div>
   );
