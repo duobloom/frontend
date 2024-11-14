ea74cb022774bcd253847f44502204771bf1cc52
potatoj1n <appabomul@gmail.com>
2024-11-15 00:05:17 +0900

    feat : 병원 상세 정보 요청에 zod 헬퍼 함수 적용한 훅 생성(#18)

--- src/apis/hospital/getHospitalInfo.ts
+++ src/apis/hospital/getHospitalInfo.ts
@@ -4,5 +4,5 @@ import axios from "axios";
 export const getHospitaInfo = async (hospitalId?: number) => {
   const response = await axios.get<HospitalType>(`/api/hospitals/${hospitalId}`);
   console.log(response.data);
-  return response.data;
+  return response;
 };
