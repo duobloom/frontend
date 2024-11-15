adc8710d3a41a0cf31a22c2b5257b3997528ea9d
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-15 15:01:15 +0900

    Refactor: 타입 수정에 따른 CommentBox 코드 수정 (#24)

--- src/components/common/CommentBox.tsx
+++ src/components/common/CommentBox.tsx
@@ -23,12 +23,12 @@ const CommentBox = ({ commentData }: TCommentBoxProps) => {
       <div className="mb-[1.5rem] mt-[2rem] h-[.6rem] w-full bg-gray-100" />
       <article className={`flex flex-col gap-[2rem] px-[1.5rem]`}>
         {commentData.map((comment) => (
-          <div key={comment.comment_id} className="flex flex-col gap-[.5rem]">
+          <div key={comment.id} className="flex flex-col gap-[.5rem]">
             <div className="flex items-center justify-between">
               <Author
                 variant="community"
-                profileImg={comment.author.profileImage}
-                name={comment.author.name}
+                profileImg={comment.profilePictureUrl}
+                name={comment.nickname}
                 createdAt={comment.createdAt}
               />
               <Drawer>
@@ -37,8 +37,8 @@ const CommentBox = ({ commentData }: TCommentBoxProps) => {
                 </DrawerTrigger>
                 <DrawerContent className="h-[23%]">
                   <div className="flex flex-col gap-[1.8rem] px-[9rem] py-[3.9rem] text-[1.6rem] font-extrabold leading-normal tracking-[-0.032rem]">
-                    <button onClick={() => handleCommentEdit(comment.comment_id)}>수정</button>
-                    <button onClick={() => handleCommentDelete(comment.comment_id)}>삭제</button>
+                    <button onClick={() => handleCommentEdit(comment.id)}>수정</button>
+                    <button onClick={() => handleCommentDelete(comment.id)}>삭제</button>
                   </div>
                 </DrawerContent>
               </Drawer>
