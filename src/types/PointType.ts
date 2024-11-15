5cbc7d2524bba53f1aa476cc242882d1d63f4c69
potatoj1n <appabomul@gmail.com>
2024-11-15 10:52:13 +0900

    Refactor : 포인트 거래내역 스키마 변경( mine boolean 데이터 추가) (#26)

--- src/types/PointType.ts
+++ src/types/PointType.ts
@@ -4,9 +4,9 @@ export const PointSchema = z.object({
   transactionId: z.number().int(),
   amount: z.number().int(),
   transactionType: z.enum(["ANSWER", "CHALLENGE_REWARD"]),
-  createdAt: z.string().datetime(),
+  createdAt: z.string(),
   balance: z.number(),
-  userId: z.number().int(),
+  mine: z.boolean().default(true),
 });
 
 export type PointType = z.infer<typeof PointSchema>;
