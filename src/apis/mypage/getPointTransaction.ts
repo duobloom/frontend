6a3af0ba8db273cc1d6d0cf078d074a575676269
potatoj1n <appabomul@gmail.com>
2024-11-15 10:53:44 +0900

    feat : zod 헬퍼 함수를 사용한 포인트 거래내역 요청 훅 생성(#26)

--- src/apis/mypage/getPointTransaction.ts
+++ src/apis/mypage/getPointTransaction.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 
 export const getPointTransaction = async () => {
   const response = await axios.get<PointType[]>("/api/points/history");
-
   console.log(response.data);
-  return response.data;
+  return response;
 };
