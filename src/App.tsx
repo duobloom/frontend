3af437f8bc7357862aaed95029d89ff8bbf6c02d
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-17 02:13:13 +0900

    Chore: react-query-devtools 라이브러리 설치 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import axios from "axios";
 import { isMobile } from "react-device-detect";
 import Navbar from "./components/layout/Nav";
@@ -42,6 +43,7 @@ function App() {
           </div>
         </div>
       </div>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
