d189a5a6ed136198223466e6495e3f6b1ba56ba3
potatoj1n <appabomul@gmail.com>
2024-11-16 10:30:54 +0900

    fix : 병원 검색 요청 API 경로 수정(#53 #18)

--- src/apis/hospital/getSearchHospital.ts
+++ src/apis/hospital/getSearchHospital.ts
@@ -2,7 +2,7 @@ import { HospitalListType } from "@/types";
 import axios from "axios";
 
 export const getSearchHospital = async (keyword?: string) => {
-  const response = await axios.get<HospitalListType[]>(`/api/hospitals/search/name=${keyword}`);
+  const response = await axios.get<HospitalListType[]>(`/api/hospitals/search?name=${keyword}`);
   console.log(response.data);
   return response;
 };
