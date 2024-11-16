aac4b276cc6a24357eaa5f5d36397560af2e600d
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-16 16:28:42 +0900

    Refactor: BasePostSchema mine, owner 타입 수정 (#39)

--- src/types/BasePostType.ts
+++ src/types/BasePostType.ts
@@ -4,7 +4,8 @@ import { CommentSchema } from "./CommentType";
 export const BasePostSchema = z.object({
   authorNickname: z.string(),
   authorProfilePictureUrl: z.string(),
-  mine: z.boolean().default(false),
+  mine: z.boolean().default(false).optional(),
+  owner: z.boolean().default(false).optional(),
   content: z.string(),
   photoUrls: z.array(z.string().url()).optional().default([]),
   createdAt: z.string().optional(),
