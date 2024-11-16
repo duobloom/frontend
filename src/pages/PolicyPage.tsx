550dda5f132fff423e001d8737f5c3948693ccba
potatoj1n <appabomul@gmail.com>
2024-11-16 21:55:23 +0900

    feat : 정책 리스트 페이지 훅 적용 및 데이터 적용 수정(#22)

--- src/pages/PolicyPage.tsx
+++ src/pages/PolicyPage.tsx
@@ -1,48 +1,101 @@
-import { useState } from "react";
-import { Drawer, DrawerContent, DrawerTrigger, DrawerTitle, DrawerClose } from "@/components/common/Drawer";
+import { useRef, useState } from "react";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/common/Drawer";
 import Header from "@/components/layout/Header";
 import { BoxFooter, DropdownBox } from "@/components/ui/Box";
-import { PolicyOptions, regions } from "@/constants";
-import { Button, ScrollableOptions } from "@/components/common";
-import { RegionSelecter } from "@/components/hospital";
-import { examplePolicyData, PolicyInfo } from "@/components/policy/PolicyInfoBox";
+import { PolicyOptions } from "@/constants";
+import { Button, InfoBox, ScrollableOptions } from "@/components/common";
+import { GetRegionName, RegionSelecter } from "@/components/hospital";
+import { useGetFilterPolicy } from "@/hooks/useGetFilterPolicy";
+import useDraggable from "@/hooks/useDraggable";
+import { cn } from "@/utils";
 
 const PolicyPage = () => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const draggableOptions = useDraggable(scrollRef);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
   const [selectedOption, setSelectedOption] = useState(1);
-  const [selectedSi, setSelectedSi] = useState<number | null>(1);
+  const [selectedSi, setSelectedSi] = useState<number | null>(null);
   const [selectedGun, setSelectedGun] = useState<number | null>(null);
   const [selectedNeighborhood, setSelectedNeighborhood] = useState<number | null>(null);
 
-  const selectedLocation = `${selectedSi ? regions.find((r) => r.id === selectedSi)?.si : ""} ${
-    selectedGun ? "· " + regions.find((r) => r.id === selectedSi)?.gun.find((g) => g.id === selectedGun)?.name : ""
-  } ${
-    selectedNeighborhood
-      ? "· " +
-        regions
-          .find((r) => r.id === selectedSi)
-          ?.gun.find((g) => g.id === selectedGun)
-          ?.neighborhoods.find((n) => n.id === selectedNeighborhood)?.name
-      : ""
-  }`;
+  const regionCode = selectedSi === 0 ? null : selectedSi;
+  const middleCode = selectedGun === 0 ? null : selectedGun;
+  const detailCode = selectedNeighborhood === 0 ? null : selectedNeighborhood;
+
+  const selectedOptionName = PolicyOptions.find((option) => option.id === selectedOption)?.name;
+  const optionKeyword: string | null = selectedOptionName === "전체" ? null : selectedOptionName || null;
+
+  const { data: policyData, refetch: refetchPolilcy } = useGetFilterPolicy(
+    regionCode,
+    middleCode,
+    detailCode,
+    optionKeyword,
+  );
+
+  const applyFilters = () => {
+    refetchPolilcy().finally(() => {
+      setIsDrawerOpen(false);
+    });
+  };
 
   return (
     <div className="flex w-full flex-col">
       <Header variant="titleMove" title="맞춤 정책" />
-      <Drawer>
-        <DrawerTrigger className="w-full">
-          <DropdownBox className="w-full">{selectedLocation || "지역 선택"}</DropdownBox>
-        </DrawerTrigger>
+      <Drawer open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+        <span className="w-full px-[1.5rem]">
+          <DrawerTrigger className="w-full" onClick={() => setIsDrawerOpen(true)}>
+            <DropdownBox className="w-full">
+              {selectedGun ? (
+                <GetRegionName
+                  selectedSi={selectedSi}
+                  selectedGun={selectedGun}
+                  selectedNeighborhood={selectedNeighborhood}
+                />
+              ) : (
+                "전체"
+              )}
+            </DropdownBox>
+          </DrawerTrigger>
+        </span>
         <ScrollableOptions options={PolicyOptions} selectedOption={selectedOption} onSelect={setSelectedOption} />
         <div className="px-[1.5rem]">
-          <p className="mt-[1rem] text-[1.5rem] font-medium">190{}개의 정책</p>
+          <p className="text-[1.5rem] font-medium">{policyData && `${policyData.length}개의 정책`}</p>
           <BoxFooter />
-          <PolicyInfo item={examplePolicyData} />
+          <div
+            ref={scrollRef}
+            {...draggableOptions()}
+            className={cn(
+              "flex flex-col gap-[1rem] overflow-y-auto scrollbar-hide",
+              "h-[47.8rem]",
+              "lg:h-[46rem]",
+              "notebook:h-[47.5rem]",
+              "2xl:h-[60rem]",
+              "lg_mobile:h-[46rem]",
+              "sm_mobile:h-[41rem]",
+              "xsm_mobile:h-[25rem]",
+            )}
+          >
+            {policyData &&
+              policyData.length > 0 &&
+              policyData.map((item) => (
+                <InfoBox
+                  key={item.policyId}
+                  variant="policy"
+                  policyId={item.policyId}
+                  policyName={item.policyName}
+                  policyHost={item.policyHost}
+                  target={item.target}
+                  region={item.region}
+                  middle={item.middle}
+                  detail={item.detail}
+                  linkUrl={item.linkUrl}
+                  keyword={item.keyword}
+                />
+              ))}
+          </div>
         </div>
         <DrawerContent>
-          <span className="relative mb-[3.5rem] flex w-full items-center">
-            <DrawerClose className="absolute left-0" />
-            <DrawerTitle text="지역 선택" className="mx-auto" />
-          </span>
           <RegionSelecter
             selectedSi={selectedSi}
             setSelectedSi={setSelectedSi}
@@ -54,7 +107,7 @@ const PolicyPage = () => {
 
           <span className="fixed bottom-[1rem] left-0 w-full px-[2rem]">
             <BoxFooter />
-            <Button variant="default" className="w-full">
+            <Button variant="default" className="w-full" onClick={applyFilters}>
               적용하기
             </Button>
           </span>
