0e331b3cf39a65ab2ece86ac8976184083f39055
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-16 16:29:09 +0900

    Refactor: CommunitySchema type enum 수정 (#39)

--- src/types/CommunityType.ts
+++ src/types/CommunityType.ts
@@ -2,13 +2,13 @@ import { z } from "zod";
 import { BasePostSchema } from "./BasePostType";
 
 export const CommunitySchema = BasePostSchema.extend({
-  community_id: z.number(),
-  type: z.enum(["심리케어", "멘토링", "정책", "병원/클리닉", "자유"]),
+  communityId: z.number(),
+  type: z.enum(["mental", "mentoring", "policy", "hospital", "free"]),
   tags: z.array(z.string()),
 });
 
 export const CommunityPostFormSchema = z.object({
-  type: z.enum(["심리케어", "멘토링", "정책", "병원/클리닉", "자유"]),
+  type: z.enum(["mental", "mentoring", "policy", "hospital", "free"]),
   content: z.string().min(1, "내용을 입력해주세요"),
   tags: z.array(z.string()).default([]),
   photoUrls: z.array(
@@ -21,4 +21,4 @@ export const CommunityPostFormSchema = z.object({
 
 export type CommunityType = z.infer<typeof CommunitySchema>;
 export type CommunityPostFormType = z.infer<typeof CommunityPostFormSchema>;
-export type CategoryType = "심리케어" | "멘토링" | "정책" | "병원/클리닉" | "자유";
+export type CategoryType = "mental" | "mentoring" | "policy" | "hospital" | "free";
