e94df18f0e080157bd3ce1d5209f57a79510f604
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-17 23:25:53 +0900

    Feat: 커뮤니티 Request 타입 추가 (#39)

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,6 +8,7 @@ import { UserType, UserProfileType, PatchUserType } from "@/types/UserType";
 import { PointType } from "./PointType";
 import { LoginType } from "./LoginType";
 import { FeedResponseType } from "@/types/FeedType";
+import { CommunityRequestType } from "@/types/CommunityType";
 
 export {
   type HospitalType,
@@ -28,4 +29,5 @@ export {
   type LoginType,
   type BoardRequestType,
   type FeedResponseType,
+  type CommunityRequestType,
 };
