6fb3df836b380fb79bb457372cd066ac8d6caf3c
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-17 20:47:34 +0900

    Refactor: 커뮤니티 데이터 타입 수정 (#30 #39)

--- src/types/CommunityType.ts
+++ src/types/CommunityType.ts
@@ -1,16 +1,30 @@
 import { z } from "zod";
-import { BasePostSchema } from "./BasePostType";
 
-export const CommunitySchema = BasePostSchema.extend({
-  community_id: z.number(),
-  type: z.enum(["심리케어", "멘토링", "정책", "병원/클리닉", "자유"]),
-  tags: z.array(z.string()),
+const tagSchema = z.object({
+  tagId: z.number(),
+  name: z.string(),
+});
+
+export const CommunitySchema = z.object({
+  communityId: z.number(),
+  type: z.enum(["MENTAL", "MENTORING", "POLICY", "HOSPITAL", "FREE"]),
+  content: z.string(),
+  nickname: z.string(),
+  profilePictureUrl: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  imageUrls: z.array(z.string()).default([]),
+  likeCount: z.number(),
+  likedByUser: z.boolean().default(false),
+  commentCount: z.number(),
+  owner: z.boolean().default(false),
+  tags: z.array(tagSchema).default([]),
 });
 
 export const CommunityPostFormSchema = z.object({
-  type: z.enum(["심리케어", "멘토링", "정책", "병원/클리닉", "자유"]),
+  type: z.enum(["MENTAL", "MENTORING", "POLICY", "HOSPITAL", "FREE"]),
   content: z.string().min(1, "내용을 입력해주세요"),
-  tags: z.array(z.string()).default([]),
+  tags: z.array(tagSchema).default([]),
   photoUrls: z.array(
     z.object({
       photo_url: z.string(),
@@ -19,6 +33,9 @@ export const CommunityPostFormSchema = z.object({
   ),
 });
 
+export const CommunityListSchema = z.array(CommunitySchema);
+
 export type CommunityType = z.infer<typeof CommunitySchema>;
+export type CommunityListType = z.infer<typeof CommunityListSchema>;
 export type CommunityPostFormType = z.infer<typeof CommunityPostFormSchema>;
-export type CategoryType = "심리케어" | "멘토링" | "정책" | "병원/클리닉" | "자유";
+export type CategoryType = "MENTAL" | "MENTORING" | "POLICY" | "HOSPITAL" | "FREE";
