4baf4de9f3781eea31ce90ab885ac3b053ff9021
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-18 22:45:31 +0900

    Feat: 에러 핸들러 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import { Outlet } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import axios from "axios";
 import { isMobile } from "react-device-detect";
-import Navbar from "./components/layout/Nav";
-import { cn } from "./utils";
+import { createQueryClient } from "@/configs/queryClient";
+import Navbar from "@/components/layout/Nav";
+import { cn } from "@/utils";
 import { IconLogoDoubloom } from "@/assets/icon";
 
-const queryClient = new QueryClient();
-
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_APP_API_ENDPOINT;
-
 function App() {
+  const queryClient = createQueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <div className={`flex h-dvh items-center justify-center bg-[#fff5f5]`}>
---
+++ src/apis/axios.ts
@@ -0,0 +1,39 @@
+// api/axios.ts
+import axios from "axios";
+import { AuthenticationError, NetworkError, NotFoundError, ServerError } from "./errors";
+
+const apiClient = axios.create({
+  withCredentials: true,
+  // baseURL: import.meta.env.VITE_APP_API_ENDPOINT
+});
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // 서버로부터 응답이 있는 경우
+    if (error.response) {
+      const { status } = error.response;
+
+      switch (status) {
+        case 401:
+          throw new AuthenticationError("로그인 해주시기 바랍니다.");
+        case 404:
+          throw new NotFoundError("찾을 수 없습니다.");
+        case 500:
+          throw new ServerError("서버 에러가 발생했습니다.");
+        default:
+          if (status >= 500) {
+            throw new ServerError("예기치 않은 서버 에러가 발생했습니다.");
+          }
+      }
+    }
+    // 네트워크 에러 (서버에 요청이 도달하지 못한 경우)
+    else if (error.request) {
+      throw new NetworkError("네트워크 연결에 실패했습니다.");
+    }
+    // 그 외의 에러
+    throw error;
+  },
+);
+
+export default apiClient;
