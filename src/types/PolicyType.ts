0dfd4a35b9a507cc0205a578e5ab80ddb5df664e
potatoj1n <appabomul@gmail.com>
2024-11-20 02:41:58 +0900

    Refactor : 정책 상세 페이지 스키마 수정(#22)

--- src/types/PolicyType.ts
+++ src/types/PolicyType.ts
@@ -4,6 +4,29 @@ const PolicyKeywordSchema = z.object({
   keyword: z.string(),
 });
 
+const PolicyTargetSchema = z.object({
+  benefit: z.union([z.string(), z.array(z.string())]), // 문자열 또는 배열 허용
+  target: z.union([z.string(), z.array(z.string())]),
+  type: z.union([z.string(), z.array(z.string())]),
+});
+
+const PolicyBenefitSchema = z.object({
+  info: z.array(z.string()),
+  method: z.object({
+    type: z.string().optional(),
+    details: z.array(z.string()).optional(),
+    application: z
+      .object({
+        online: z.string().optional(),
+      })
+      .optional(),
+    onlineApplication: z.string().optional(),
+    requiredDocuments: z.array(z.string()).optional(),
+  }),
+  target: z.union([z.string(), z.array(z.string())]),
+  content: z.string(),
+});
+
 // 정책 전체 타입
 export const PolicySchema = z.object({
   policyId: z.number(),
@@ -14,7 +37,8 @@ export const PolicySchema = z.object({
   detail: z.string().nullable(),
   startDate: z.string().nullable(),
   endDate: z.string().nullable(),
-  target: z.string().nullable(),
+  target: PolicyTargetSchema.nullable(),
+  benefit: PolicyBenefitSchema.nullable(),
   linkUrl: z.string(),
   imageUrl: z.string().nullable(),
   scraped: z.boolean(),
