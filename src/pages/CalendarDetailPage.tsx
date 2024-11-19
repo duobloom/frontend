fe1ec3ef0a95c104c2db99bf99f4e6ab2f5fc357
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-20 00:42:26 +0900

    Design: 캘린더 상세 페이지 스타일 수정 (#28)

--- src/pages/CalendarDetailPage.tsx
+++ src/pages/CalendarDetailPage.tsx
@@ -35,7 +35,8 @@ const CalendarDetailPage = () => {
         {day} {weekDay}의 <br />
         기록을 알아보세요
       </h1>
-      <section className="flex h-[calc(100%-18.8rem)] flex-col gap-[1.5rem] overflow-auto rounded-t-[3rem] bg-gray-100 bg-[linear-gradient(to_right,_transparent,_transparent_50%,_#E2E2E2_50%,_#E2E2E2_50.3%,_transparent_50.3%)] bg-[length:100%_1px] bg-center p-[1.5rem] shadow-feed scrollbar-hide">
+      <section className="relative z-0 flex h-[calc(100%-12.3rem)] flex-col gap-[1.5rem] overflow-auto rounded-t-[3rem] bg-gray-100 px-[1.5rem] pb-[1.5rem] pt-[1.5rem] shadow-feed scrollbar-hide">
+        <div className="absolute left-1/2 top-0 h-full w-[.1rem] bg-gray-300" />
         {isLoading ? (
           <>로딩 중...</>
         ) : (
