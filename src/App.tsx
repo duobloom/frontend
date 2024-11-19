e8ac453cc479eb3f27960ffd973141f31fa1e5a3
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-11-19 23:21:47 +0900

    Feat: App 컴포넌트 ToastProvider 입히기 (#75)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { isMobile } from "react-device-detect";
 import Navbar from "./components/layout/Nav";
 import { cn } from "./utils";
+import { ToastProvider, useToastContext } from "./libs/custom-toast";
 import { IconLogoDoubloom } from "@/assets/icon";
 
 const queryClient = new QueryClient();
@@ -13,6 +14,8 @@ axios.defaults.withCredentials = true;
 // axios.defaults.baseURL = import.meta.env.VITE_APP_API_ENDPOINT;
 
 function App() {
+  useToastContext(); // context 설정을 위해 추가
+
   return (
     <QueryClientProvider client={queryClient}>
       <div className={`flex h-dvh items-center justify-center bg-[#fff5f5]`}>
@@ -32,14 +35,16 @@ function App() {
           </div>
           {/* Right section - 앱 영역 */}
           <div className="right-section relative flex w-full justify-center lg:w-[37.5rem] lg:min-w-[37.5rem]">
-            <div className={cn("h-dvh w-full bg-white shadow-lg", isMobile ? "w-full" : "max-w-[37.5rem]")}>
-              <div className="flex h-full flex-col">
-                <div className="flex-1 overflow-y-auto pb-[6.5rem] will-change-scroll">
-                  <Outlet />
+            <ToastProvider autoClose={1000} limit={3}>
+              <div className={cn("h-dvh w-full bg-white shadow-lg", isMobile ? "w-full" : "max-w-[37.5rem]")}>
+                <div className="flex h-full flex-col">
+                  <div className="flex-1 overflow-y-auto pb-[6.5rem] will-change-scroll">
+                    <Outlet />
+                  </div>
+                  <Navbar />
                 </div>
-                <Navbar />
               </div>
-            </div>
+            </ToastProvider>
           </div>
         </div>
       </div>
