bc3d30939efdd8d90b036cd85974fea797cedcf6
윤태현 <yoonth0919@gmail.com>
2024-11-21 00:05:26 +0900

    Refactor: 지도 페이지일 경우 스타일 동적 변경 (#18)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import axios from "axios";
@@ -14,6 +14,8 @@ axios.defaults.withCredentials = true;
 axios.defaults.baseURL = import.meta.env.VITE_APP_API_ENDPOINT;
 
 function App() {
+  const location = useLocation();
+  const pathname = location.pathname;
   return (
     <QueryClientProvider client={queryClient}>
       <div className={`flex h-dvh items-center justify-center bg-[#fff5f5]`}>
@@ -36,7 +38,9 @@ function App() {
             <ToastProvider autoClose={1000} limit={3}>
               <div className={cn("h-dvh w-full bg-white shadow-lg", isMobile ? "w-full" : "max-w-[37.5rem]")}>
                 <div className="flex h-full flex-col">
-                  <div className="flex-1 overflow-y-auto will-change-scroll">
+                  <div
+                    className={`flex-1 overflow-y-auto ${pathname.includes("map") || "pb-[6.5rem]"} will-change-scroll`}
+                  >
                     <Outlet />
                   </div>
                   <Navbar />
