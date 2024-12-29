9b36eefa0d84f2522c369e93c6f6c457eeae1789
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-12-29 22:45:11 +0900

    Refactor: vite.config.ts파일 Sentry authToken 환경변수 적용 (#89)

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import path from "path";
@@ -6,58 +6,62 @@ import tailwindcss from "tailwindcss";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    sentryVitePlugin({
-      org: "duobloom",
-      project: "duobloom-sentry",
-      sourcemaps: {
-        filesToDeleteAfterUpload: "./dist/**/*.map",
-      },
-      authToken: "secret",
-    }),
-    svgr({ include: "**/*.svg?react" }),
-  ],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://ec2-52-78-212-34.ap-northeast-2.compute.amazonaws.com:8080",
-        changeOrigin: true,
-      },
-      "/file": {
-        target: "http://ec2-52-78-212-34.ap-northeast-2.compute.amazonaws.com:8080",
-        changeOrigin: true,
-      },
-      "/IMAGE": {
-        target: "http://ec2-52-78-212-34.ap-northeast-2.compute.amazonaws.com:8080",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ""); // vite에서 env 사용
+
+  return {
+    plugins: [
+      react(),
+      sentryVitePlugin({
+        org: "duobloom",
+        project: "duobloom-sentry",
+        sourcemaps: {
+          filesToDeleteAfterUpload: "./dist/**/*.map",
+        },
+        authToken: env.VITE_APP_SENTRY_AUTH_TOKEN,
+      }),
+      svgr({ include: "**/*.svg?react" }),
+    ],
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
       },
     },
-  },
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("icon-")) {
-            return `icons/${id.split("/").pop()?.replace(".svg?react", "")}`;
-          }
-          if (id.includes("@sentry")) {
-            return "sentry";
-          }
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: "http://ec2-52-78-212-34.ap-northeast-2.compute.amazonaws.com:8080",
+          changeOrigin: true,
+        },
+        "/file": {
+          target: "http://ec2-52-78-212-34.ap-northeast-2.compute.amazonaws.com:8080",
+          changeOrigin: true,
+        },
+        "/IMAGE": {
+          target: "http://ec2-52-78-212-34.ap-northeast-2.compute.amazonaws.com:8080",
+          changeOrigin: true,
         },
       },
     },
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes("icon-")) {
+              return `icons/${id.split("/").pop()?.replace(".svg?react", "")}`;
+            }
+            if (id.includes("@sentry")) {
+              return "sentry";
+            }
+          },
+        },
+      },
 
-    sourcemap: true,
-  },
+      sourcemap: true,
+    },
+  };
 });
