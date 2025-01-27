b3c71f28c3f0d46cc56c22ccaf97756b5765c741
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-01-27 18:30:49 +0900

    Feat: LazyIcon 컴포넌트 메모이제이션 적용 (#87)

--- src/assets/icon/LazyIcon.tsx
+++ src/assets/icon/LazyIcon.tsx
@@ -1,19 +1,19 @@
 // SVG 컴포넌트 Lazy Loading
 
-import React, { Suspense } from "react";
+import React, { memo, Suspense, useMemo } from "react";
 
 type LazyIconProps = {
   name: string;
 } & React.SVGProps<SVGSVGElement>;
 
-const LazyIcon = ({ name, ...rest }: LazyIconProps) => {
-  const IconComponent = React.lazy(() => import(`./${name}.svg?react`));
+const LazyIcon = memo(({ name, ...rest }: LazyIconProps) => {
+  const IconComponent = useMemo(() => React.lazy(() => import(`./${name}.svg?react`)), [name]);
 
   return (
     <Suspense fallback={<div className="h-full w-full animate-pulse bg-gray-100" />}>
       <IconComponent {...rest} />
     </Suspense>
   );
-};
+});
 
 export default LazyIcon;
